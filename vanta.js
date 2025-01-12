03af3d38890c4217905da3f9bac409d79993d2c3
Amaryadav07 <anath2893007@gmail.com>
2025-01-12 14:23:05 +0530

    updated code

--- vanta.js
+++ vanta.js
@@ -8,16 +8,7 @@ VANTA.NET({
     scale: 1.00,
     scaleMobile: 1.00
   })
-  VANTA.NET({
-    el: "#car-details",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 300.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00
-  })
+ 
  
 
   VANTA.DOTS({
