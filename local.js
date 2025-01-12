9ad22641ac22bd7b4ad8ad2a64a86c2dc8d8ce6d
Amaryadav07 <anath2893007@gmail.com>
2025-01-12 13:50:17 +0530

    first commit

---
+++ local.js
@@ -0,0 +1,224 @@
+let signup = () => {
+   
+    let signname = document.querySelector("#name").value;
+    let signmobile = document.querySelector("#mobile").value;
+    let signemail = document.querySelector("#email").value;
+    let signpassword = document.querySelector("#password").value;
+
+    let errorname = document.querySelector("#name");
+    let errormobile = document.querySelector("#mobile");
+    let erroremail = document.querySelector("#email");
+    let errorpassword = document.querySelector("#password");
+
+    errorname.setAttribute("placeholder", "");
+    errormobile.setAttribute("placeholder", "");
+    erroremail.setAttribute("placeholder", "");
+    errorpassword.setAttribute("placeholder", "");
+
+   
+    if (signname === "") {
+        errorname.setAttribute("placeholder", "Please Enter your Name");
+        errorname.style.borderColor = "red";
+        document.querySelector("#name").focus();
+        return false;
+    }
+
+    
+    if (isNaN(signmobile)) {
+        errormobile.setAttribute("placeholder", "Please Enter a valid Mobile Number");
+        errormobile.style.borderColor = "red";
+        document.querySelector("#mobile").focus();
+        return false;
+    } else if (signmobile === "") {
+        errormobile.setAttribute("placeholder", "Please Enter your Mobile");
+        errormobile.style.borderColor = "red";
+        document.querySelector("#mobile").focus();
+        return false;
+    } else if (signmobile.length !== 10) {
+        errormobile.setAttribute("placeholder", "Please Enter 10 digit Mobile Number");
+        errormobile.style.borderColor = "red";
+        document.querySelector("#mobile").focus();
+        return false;
+    }
+
+  
+    if (!(signemail.includes('@') && signemail.includes('.com'))) {
+        erroremail.setAttribute("placeholder", "Please Enter a Valid Email");
+        erroremail.style.borderColor = "red";
+        document.querySelector("#email").focus();
+        return false;
+    } else if (signemail === "") {
+        erroremail.setAttribute("placeholder", "Please Enter your Email");
+        erroremail.style.borderColor = "red";
+        document.querySelector("#email").focus();
+        return false;
+    }
+
+   
+    if (!(signpassword.match(/[1234567890]/) &&
+        signpassword.match(/[!@#$%^&*()]/) &&
+        signpassword.match(/[a-z]/) &&
+        signpassword.match(/[A-Z]/))) {
+        document.querySelector("#password").focus();
+        alert("Password should have at least one uppercase letter, one lowercase letter, one digit");
+        return false;
+    } else if (signpassword === "") {
+        errorpassword.setAttribute("placeholder", "Please Enter your Password");
+        errorpassword.style.borderColor = "red";
+        document.querySelector("#password").focus();
+        return false;
+    }
+
+  
+    localStorage.setItem("name", signname);
+    localStorage.setItem("mobile", signmobile);
+    localStorage.setItem("email", signemail);
+    localStorage.setItem("password", signpassword);
+
+   
+    location.href = "locallogin.html";
+    return false; 
+}
+
+let login=()=>{
+
+    let loginname = document.querySelector("#name").value;
+    let loginpassword = document.querySelector("#password").value;
+
+    let errornamelogin = document.querySelector("#name");
+    let errorpasswordlogin = document.querySelector("#password");
+
+    errornamelogin.setAttribute("placeholder", "");
+    errorpasswordlogin.setAttribute("placeholder", "");
+
+    if (loginname === "") {
+        errornamelogin.setAttribute("placeholder", "Please Enter your Name");
+        errornamelogin.style.borderColor = "red";
+        document.querySelector("#name").focus();
+        return false;
+    }
+
+    if (!(loginpassword.match(/[1234567890]/) &&
+    loginpassword.match(/[!@#$%^&*()]/) &&
+    loginpassword.match(/[a-z]/) &&
+    loginpassword.match(/[A-Z]/))) {
+    document.querySelector("#password").focus();
+    alert("Password should have at least one uppercase letter, one lowercase letter, one digit");
+    return false;
+
+}
+ else if (loginpassword === "") {
+    errorpasswordlogin.setAttribute("placeholder", "Please Enter your Password");
+    errorpasswordlogin.style.borderColor = "red";
+    document.querySelector("#password").focus();
+    return false;
+}
+
+let storedName = localStorage.getItem("name");
+let storedPassword = localStorage.getItem("password");
+
+
+if (loginname === storedName && loginpassword === storedPassword) {
+    alert("Login Successful");
+    location.href = "index.html"; 
+} else {
+    alert("Login credentials do not match!");
+}
+return false
+}
+
+// hamburger menu 
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navbarLinks = document.getElementById('navbar-links');
+
+   
+    hamburgerMenu.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active');
+    });
+});
+
+
+// home
+
+let isLoggedIn = false;
+
+const loginButton = document.getElementById('login-btn');
+const logoutButton = document.getElementById('logout-btn');
+
+
+loginButton.addEventListener('click', () => {
+    isLoggedIn = true; 
+    updateAuthButtons(); 
+});
+
+
+logoutButton.addEventListener('click', () => {
+    isLoggedIn = false; 
+    updateAuthButtons(); // Update button visibility
+    localStorage.clear()
+});
+
+
+function updateAuthButtons() {
+    if (isLoggedIn) {
+        loginButton.style.display = 'none'; 
+        logoutButton.style.display = 'inline-block'; 
+    } else {
+        loginButton.style.display = 'inline-block'; 
+        logoutButton.style.display = 'none'; 
+    }
+}
+
+// slider code
+
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+//   booking form 
+document.getElementById('booking-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+  
+    const pickupLocation = document.getElementById('pickup-location').value;
+    const dropoffLocation = document.getElementById('dropoff-location').value;
+    const pickupDate = document.getElementById('pickup-date').value;
+    const dropoffDate = document.getElementById('dropoff-date').value;
+
+    
+    const bookingData = {
+      pickupLocation,
+      dropoffLocation,
+      pickupDate,
+      dropoffDate
+    };
+
+    
+    localStorage.setItem('bookingData', JSON.stringify(bookingData));
+
+
+    alert('Booking Confirmed and Saved!');
+  });
+
+// typed js
+// var typed = new Typed('#new', {
+//     strings: ["Premium"," Luxury"],
+//     typeSpeed: 200,
+//     loop:true
+//   });
+ 
---
+++ vanta.js
@@ -0,0 +1,55 @@
+VANTA.NET({
+    el: "#booking-form",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 300.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
+  VANTA.NET({
+    el: "#car-details",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 300.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
+ 
+
+  VANTA.DOTS({
+    el: "#reviews",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
+
+  VANTA.CLOUDS({
+    el: "#filters",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 100.00,
+    minWidth: 200.00
+  })
+ 
+
+ 
+  
+  VANTA.DOTS({
+    el: "#car-types",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
